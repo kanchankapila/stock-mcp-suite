b6c222bf7230bbf54fa4b953d5dcfc07e8566a10
Kapila, Amit <kanchan.kapila.2020@gmail.com>
2025-09-19 18:31:21 +0530

    Revert "Implement MoneyControl API integration - Phase 1: Indices and Stock Data providers with frontend cards"
    This reverts commit 3e1bfcf79093e9193d28a9f98fc49894a2bf18b3.

--- server/src/providers/moneycontrol/index.ts
+++
@@ -1,6 +0,0 @@
-export { IndicesProvider } from './indices.provider';
-export { StocksProvider } from './stocks.provider';
-
-// Provider instances
-export const indicesProvider = new IndicesProvider();
-export const stocksProvider = new StocksProvider();
